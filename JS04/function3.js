87dcfac2ec93b5c1dd33e902a3d5011bc7f28560
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-07 16:24:05 +0600

    september 7 function3

---
+++ JS04/function3.js
@@ -0,0 +1,17 @@
+// Example 1
+function add(a, b) {
+    return a + b;
+  }
+  
+  let result = add(5, 3);
+  console.log(result);
+
+  
+// Example 2
+let multiply = function (x, y) {
+    return x * y;
+}
+
+let final = multiply (5, 2);
+
+console.log (final);
