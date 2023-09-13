ee26c4d3cc230ef3cf45a5cf498ffdc5bebf5787
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-13 13:51:11 +0600

    revisioning function

---
+++ JS04/function4.js
@@ -0,0 +1,14 @@
+// Example 1
+function add (a, b, c){
+    console.log (a + b + c);
+}
+
+add (2, 3, 5);
+
+// Example 2
+function newAdd (num1, num2, num3){
+    return num1 + num2 + num3;
+}
+
+let sum = newAdd (5, 10, 5);
+console.log (sum);
