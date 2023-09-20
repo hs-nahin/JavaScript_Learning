f21cad0f277a2498a3cc9a4de221e351acb7f939
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-20 23:43:05 +0600

    new practice

---
+++ JS04/function5.js
@@ -0,0 +1,10 @@
+let a = myNumber (10, 20);
+let b = myNumber (10, 20);
+let c = myNumber (10, 20);
+let d = myNumber (10, 20);
+
+function myNumber(a, b){
+    return (a+a) + (b+b) - 1;
+}
+
+console.log (a);
