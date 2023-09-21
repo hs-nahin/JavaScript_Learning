6ef3bd9f2dd1f001371dec4f489b37d3bc1499e9
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-21 17:58:43 +0600

    condition practicing

---
+++ JS06/condition.js
@@ -0,0 +1,23 @@
+// Scenario 1 - Single Branch
+// If Condition
+if (haveMoney){
+    purchasePhone()
+}
+
+// Scenario 2 - Two Branch
+// If Else Condition
+if (toss === head){
+    window()
+} else {
+    lose ()
+}
+
+// Scenario 3 - Multiple Branch
+// Else If Condition
+if (1 > 1){
+    big ()
+} else if (1 < 1){
+    small ()
+} else {
+    same ()
+}
