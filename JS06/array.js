e654fd363ce2dd16e8aabc5102a60d4e2ae77cb5
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-21 20:22:39 +0600

    array practice

---
+++ JS06/array.js
@@ -0,0 +1,8 @@
+const books = ["Bangla", "English", "Math", "ICT"]
+console.log (books[0]);
+console.log (books[2]);
+
+for (let i = 0; i < 4; i++){
+    console.log (books, i);
+    console.log (books[i], i);
+}
