1533d58ccd2da21e4e4ee7a2ad6ea69f01518a0f
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-24 15:50:15 +0600

    if else with AND &&

---
+++ JS06/if-else2.js
@@ -0,0 +1,31 @@
+// AND &&
+// Both are TRUE
+if (10 > 9 && 10 == 10){
+    console.log ("Subscribe");
+} else {
+    console.log ("Subscribed");
+}
+// One is False, other one is True
+if (10 > 15 && 15 == 15){
+    console.log ("Subscribe");
+} else {
+    console.log ("Subscribed");
+}
+// One is TRUE, other one is FALSE
+if (15000 > 10000 && 15 != 15){
+    console.log ("Can Purchase a Phone");
+} else {
+    console.log ("Cannot Purchase a Phone");
+}
+// Both are True
+if (15000 < 20000 && 15 == 15){
+    console.log ("Can Purchase a Phone");
+} else {
+    console.log ("Cannot Purchase a Phone");
+}
+// Both are False
+if (20 > 25 && 25 == 30){
+    console.log ("Both are True");
+} else {
+    console.log ("Both are False");
+}
