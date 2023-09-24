e82cc2293aca96438ff3556f5c23f921553fa1d8
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-24 17:52:45 +0600

    if else-if else

---
+++ JS06/ifElse-elseIf.js
@@ -0,0 +1,10 @@
+let myAge = 23;
+let sisterAge = 21;
+
+if (myAge > sisterAge){
+    console.log ("I am older");
+} else if (myAge < sisterAge) {
+    console.log ("Sister is older than me");
+} else {
+    console.log ("Both are same age");
+}
