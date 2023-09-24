0859571bbd9f761eb258bd414580a0b6bc90ffcd
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-24 15:49:11 +0600

    for loop praciice

---
+++ JS06/for-loop2.js
@@ -0,0 +1,6 @@
+for (let i = 1; i < 10; i++){
+    // Its a new JS file
+    // We can write any valid JS code here
+    // Every Code written inside this block will execute multiple times
+    console.log (i, "Salam World");
+}
