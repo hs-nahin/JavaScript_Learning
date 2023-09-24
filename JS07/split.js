e2703d191f5b347e5029edec74ad3272910f56b3
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-24 23:05:04 +0600

    split

---
+++ JS07/split.js
@@ -0,0 +1,7 @@
+const lyrics = "Amake Amar, Moto Thakte Dao. Ami Nijeke Nijer Moto Guchiye Niyechi";
+const parts = lyrics.split(" ");
+const sentence = lyrics.split(",");
+const sentence2 = lyrics.split (".");
+console.log (parts);
+console.log (sentence);
+console.log (sentence2);
