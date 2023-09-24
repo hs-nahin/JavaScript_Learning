dbf1c94241389816794b9cf4d58882ceb20931a5
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-24 15:54:34 +0600

    a little changes

--- JS06/if-else2.js
+++ JS06/if-else2.js
@@ -1,15 +1,16 @@
 // AND &&
+// Both have to be TRUE to show the "if" value or it will show the "else" value
 // Both are TRUE
 if (10 > 9 && 10 == 10){
-    console.log ("Subscribe");
+    console.log ("Subscribe"); //If both are True
 } else {
-    console.log ("Subscribed");
+    console.log ("Subscribed"); 
 }
 // One is False, other one is True
 if (10 > 15 && 15 == 15){
     console.log ("Subscribe");
 } else {
-    console.log ("Subscribed");
+    console.log ("Subscribed"); //If False
 }
 // One is TRUE, other one is FALSE
 if (15000 > 10000 && 15 != 15){
