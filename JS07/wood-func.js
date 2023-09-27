e5559eb51f1459468c3296bc328462e0a7d5c4ea
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2023-09-27 10:33:25 +0600

    wood function

---
+++ JS07/wood-func.js
@@ -0,0 +1,15 @@
+function woodCalculator (chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
+
+    const chairWood = chairQuantity * perChairWood;
+    const tableWood = tableQuantity * perTableWood;
+    const bedWood = bedQuantity * perBedWood;
+
+    const totalWood = chairWood + tableWood + bedWood;
+    return totalWood;
+}
+
+const totalwood = woodCalculator (2, 2, 5);
+console.log ("Total Wood Required : ", totalwood);
