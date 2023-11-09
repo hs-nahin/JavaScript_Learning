89edbcb32fbc54f86631f155256c160bb3119ed1
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-11-10 01:17:46 +0600

    changes in comment

--- JS08/love.js
+++ JS08/love.js
@@ -1,13 +1,13 @@
-// Create an array named 'marriage' with initial values "Nusrat" and "Jahan".
+// Create an array named 'Love' with initial values "Nusrat" and "Jahan".
 let Love = ["Nusrat", "Jahan"];
 
-// Add a new element "Mim" to the 'marriage' array.
+// Add a new element "Mim" to the 'Love' array.
 Love.push("Mim");
 
-// Use a for loop to iterate over each element in the 'marriage' array.
+// Use a for loop to iterate over each element in the 'Love' array.
 // The loop initializes with i = 0, continues as long as i is less than the length of the array, and increments i after each iteration.
 for (let i = 0; i < Love.length; i++) {
-    // Print each element of the 'marriage' array to the console.
+    // Print each element of the 'Love' array to the console.
     console.log(Love[i], i);
     console.log(Love, i);
 }
