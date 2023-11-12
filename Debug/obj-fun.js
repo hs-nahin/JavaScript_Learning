7ca1b6acc7d75ca8792059424e450823979d795b
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-11-12 12:47:32 +0600

    adding good comments

--- Debug/obj-fun.js
+++ Debug/obj-fun.js
@@ -1,18 +1,19 @@
+// An object containing personal details and functions
 const obj = {
-    name: "Hasnat",
-    middleName: "Shahriyar",
-    skills: ["React", "JavaScript", "HTML", "Tailwind"],
-    age: 23,
-    fullName: function () {
-        return this.name + " " + this.middleName;
+    name: "Hasnat", // Name property
+    middleName: "Shahriyar", // Middle name property
+    skills: ["React", "JavaScript", "HTML", "Tailwind"], // Array of skills
+    age: 23, // Age property
+    fullName: function () { // Method to generate full name
+        return this.name + " " + this.middleName; // Combines name and middleName
     },
-    printSkills: function () {
-        const fullName = this.fullName(); // Getting the full name
-        this.skills.forEach(function (value) {
-            console.log(`${fullName} has skills in ${value}`); // Updated output
+    printSkills: function () { // Method to print skills
+        const fullName = this.fullName(); // Getting the full name by calling the fullName method
+        this.skills.forEach(function (value) { // Loop through each skill in the skills array
+            console.log(`${fullName} has skills in ${value}`); // Outputting the full name and each skill
         });
     }
 };
 
-console.log(obj.fullName());
-obj.printSkills();
+console.log(obj.fullName()); // Calling fullName method and logging the result
+obj.printSkills(); // Calling the printSkills method to display each skill along with the full name
