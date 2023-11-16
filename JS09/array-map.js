85f756ea3ebb10b5a7c040b23753080ef49932ab
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-11-16 12:44:32 +0600

    adding good comments

--- JS09/array-map.js
+++ JS09/array-map.js
@@ -1,9 +1,13 @@
+// Define an array of products with details such as name, price, and brand.
 const product = [
-    {name : "laptop", price : 55000, brand : "asus"},
-    {name : "phone", price : 15000, brand : "realme"},
-    {name : "watch", price : 2000, brand : "xiaomi"},
-    {name : "camera", price : 155000, brand : "sony"},
+    {name: "laptop", price: 55000, brand: "asus"},
+    {name: "phone", price: 15000, brand: "realme"},
+    {name: "watch", price: 2000, brand: "xiaomi"},
+    {name: "camera", price: 155000, brand: "sony"},
 ];
 
+// Extract an array of brand names from the products using the map function.
 const brands = product.map(product => product.brand);
-console.log (brands);
+
+// Log the array of brand names to the console.
+console.log(brands);
