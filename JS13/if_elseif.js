77e8559ab1a57d634983070544fcf70ca3b4914f
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-04-23 19:19:58 +0600

    conditional statements

---
+++ JS13/if_elseif.js
@@ -0,0 +1,12 @@
+var age = 24;
+
+if(age<18){
+    console.log("You are underage!");
+    
+} else if(age <= 18){
+    console.log("I am now an adult");
+    
+} else{
+    console.log("I am more than just an adult");
+    
+}
