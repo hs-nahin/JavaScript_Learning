e7ea43e34752c6fba3fbefe225f89e0abb114194
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-04-25 13:17:05 +0600

    switch statements

---
+++ JS13/switch.js
@@ -0,0 +1,18 @@
+var names = 'nahin';
+switch (names) {
+    case 'nahin':
+        console.log('My name is nahin');
+        break;
+    case 'rakib':
+        console.log('My name is rakib');
+        break;
+    case 'mehedi':
+        console.log('My name is mehedi');
+        break;
+    case 'shad':
+        console.log('my name is shad');
+        break;
+    default:
+        console.log('I have no name');
+        break;
+}
