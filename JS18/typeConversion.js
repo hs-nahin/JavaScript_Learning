9d35d90a3be31194dcdfcf91a1a149cfd65ab323
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-06-09 01:39:52 +0600

    typeconversion

---
+++ JS18/typeConversion.js
@@ -0,0 +1,8 @@
+let str = '1000';
+let num = 11;
+
+console.log("String: ", str);
+console.log("Number: ", num);
+
+console.log("String Converted to Number: ",parseInt(str));
+console.log("Number Converted to String: ", num.toString());
