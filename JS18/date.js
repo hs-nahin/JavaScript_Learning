cabfa78dc6de5a1545804f1cccf0ee3bf1046f5d
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-06-09 02:04:25 +0600

    date in js

---
+++ JS18/date.js
@@ -0,0 +1,11 @@
+let date = new Date();
+
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.toLocaleString());
+
+console.log(date.getDate());
+console.log(date.getFullYear());
+console.log(date.getMonth());
