61e1961125bd7b77d524450d64f63b42e2cd6f47
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-06-11 21:11:36 +0600

    iife

---
+++ JS23/IIFE.js
@@ -0,0 +1,13 @@
+(function hello() {
+    let msg = "Hello world";
+    console.log(msg);
+}) ();
+
+
+{
+    let myName = function () {
+        let a = "Hasnat";
+        console.log(a);
+    }
+}
+myName();
