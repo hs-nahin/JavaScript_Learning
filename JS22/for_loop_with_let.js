75f1b7294c087ee710257d06ba609912671e082d
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-06-11 20:47:36 +0600

    scope and hoisting using var, let and const

---
+++ JS22/for_loop_with_let.js
@@ -0,0 +1,9 @@
+let i = 10;
+console.log("Before Loop: " + i);
+
+for (let i = 0; i < 5; i++)
+{
+    console.log("Inside Loop", + i);
+}
+
+console.log("After Loop: " + i);
---
+++ JS22/for_loop_with_var.js
@@ -0,0 +1,9 @@
+var i = 10;
+console.log("Before Loop: " + i);
+
+for (var i = 0; i < 5; i++)
+{
+    console.log("Inside Loop", + i);
+}
+
+console.log("After Loop: " + i);
