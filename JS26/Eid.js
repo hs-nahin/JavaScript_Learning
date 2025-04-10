a611956584118822d979fac7db3385a8d95ac8b9
hs-nahin <hasnatshahriyarnahin1@gmail.com>
2025-04-10 07:33:26 +0600

    js learning

---
+++ JS26/Eid.js
@@ -0,0 +1,14 @@
+let MoonSeen = true;
+
+if (MoonSeen){
+    let EidInSaudi = false;
+    let EidInChandpur = true;
+
+    if (EidInSaudi && EidInChandpur){
+        console.log("Tomorrow is Eid in whole Bangladesh"); 
+    } else {
+        console.log("Eid is in Chandpur");
+    }
+} else {
+    console.log("There wil be no Eid Tomorrow");
+}
